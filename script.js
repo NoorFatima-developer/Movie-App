ac812204a2349f5e1b97ea79fc849a1d6119ef4f
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-29 08:11:31 +0500

    code is added

--- script.js
+++ script.js
@@ -12,9 +12,35 @@ async function loadMovies(searchTerm){
     const response = await fetch(`${url}`);
     const data = await response.json();
     if(data.Response === "True"){
-        console.log(data.Search);
-    }
-    
+        displayMovieList(data.Search)
+        // console.log(data);
+        // console.log(data.Search);
+    } 
 }
 
-loadMovies("lord of the Rings")
+// findMovies:
+
+function findMovies(){
+    let searchTerm = (movieSearchBox.value).trim();
+    // console.log(searchTerm); 
+    if(searchTerm.length > 0){
+        searchList.classList.remove("hide-search-list");
+        loadMovies(searchTerm);
+    }
+    else{
+        searchList.classList.add("hide-search-list");
+    }
+}
+
+
+// display movies on the page:
+function displayMovieList(movies){
+    searchList.innerHTML = "";
+    for(let i=0; i<movies.length; i++){
+        let movieListItem = document.createElement('div');
+        console.log(movieListItem);
+        movieListItem.dataset.id = movies[i].imdbID;
+    }
+}
+
+// loadMovies("lord of the Rings")
