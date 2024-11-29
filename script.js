153a052b0c829af697af923549eb608babfdefea
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-29 07:51:45 +0500

    code is added

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-// Most popular movies
-const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-// Searched movies
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+// Titles: https://omdbapi.com/?s=thor&page=1&apikey=fc1fef96
+// details: http://www.omdbapi.com/?i=tt3896198&apikey=fc1fef96
 
+const movieSearchBox = document.getElementById('movie-search-box');
+const searchList = document.getElementById('search-list');
+const resultGrid = document.getElementById('result-grid');
+
+// load movies from API:
+
+async function loadMovies(searchTerm){
+    const url = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`;
+    const response = await fetch(`${url}`);
+    const data = await response.json();
+    console.log(data);
+    
+}
+
+loadMovies("lord of the Rings")
